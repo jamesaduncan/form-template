2619e0addc86fa7c55e1d17244f082f04485cf79
James A. Duncan <james@kinburnstreet.consulting>
2025-03-18 17:03:39 +0000

    Working on the docs a little

---
+++ index.mjs
@@ -0,0 +1,23 @@
+import SelectorSubscriber from "https://jamesaduncan.github.io/selector-subscriber/index.mjs";
+
+SelectorSubscriber.subscribe("form[template]", ( aForm ) => {
+    aForm.addEventListener('submit', async ( theEvent ) => {
+
+        const formdata    = new FormData( theEvent.target );
+        const destination = document.querySelector( theEvent.target.getAttribute('action') );
+        const template    = ( theEvent.target.hasAttribute('template') ) ? document.querySelector( theEvent.target.getAttribute('template') ) 
+                                                                         : destination.querySelector('template'); 
+
+        const clone = template.content.cloneNode( true );
+        Array.from( formdata.keys() ).forEach( (key) => {
+            try {
+                clone.querySelector(`slot[name=${key}]`).replaceWith( document.createTextNode( formdata.get( key ) ) );
+            } catch(e) {
+                console.warn(e);
+            }
+        });
+
+        theEvent.preventDefault();
+        destination[ theEvent.target.getAttribute('mutation') || "appendChild" ]( clone );
+    });
+});
