b18ffc376d35b98b89a1278c1701055587c49032
James A. Duncan <james@kinburnstreet.consulting>
2025-05-25 19:20:44 +0100

    Use the itemprop attribute as an alternate to a slot if appropriate.

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import SelectorSubscriber from "https://jamesaduncan.github.io/selector-subscrib
 
 SelectorSubscriber.subscribe("form[template]", ( aForm ) => {
     aForm.addEventListener('submit', async ( theEvent ) => {
+        theEvent.preventDefault();
         const formdata    = new FormData( theEvent.target );
         const destination = document.querySelector( theEvent.target.getAttribute('action') );
         const template    = document.querySelector( theEvent.target.getAttribute('template') );
@@ -9,13 +10,22 @@ SelectorSubscriber.subscribe("form[template]", ( aForm ) => {
         const clone = template.content.cloneNode( true );
         Array.from( formdata.keys() ).forEach( (key) => {
             try {
-                clone.querySelector(`slot[name=${key}]`).replaceWith( document.createTextNode( formdata.get( key ) ) );
+                const node = clone.querySelector(`slot[name=${key}]`);
+                if ( node ) {
+                    node.replaceWith( document.createTextNode( formdata.get( key ) ) );
+                } else {
+                    const node = clone.querySelector(`[itemprop=${key}]`);
+                    if ( node ) {
+                        node.innerHTML = formdata.get( key );
+                    } else {
+                        console.warn(`No slot or itemprop found for ${key}`);
+                    }
+                }
             } catch(e) {
                 console.warn(e);
             }
         });
 
-        theEvent.preventDefault();
         destination[ theEvent.target.getAttribute('mutation') || "appendChild" ]( clone );
         if ( theEvent.target.hasAttribute('reset') && theEvent.target.getAttribute('reset') ) theEvent.target.reset();
     });
